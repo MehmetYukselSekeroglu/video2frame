8a3c0fa156b5335e5f2b598608633c3b5d9ccfb2
/Gama <88578910+TheKoba-dev@users.noreply.github.com>
2023-03-05 21:38:13 +0300

    Add files via upload

---
+++ video2frame.py
@@ -0,0 +1,72 @@
+import cv2
+import sys
+import os
+
+OUTPUT_DIR = f"tmp{os.sep}cv2output"
+TMP_DIR = "tmp"
+
+def ShowHelp():
+    print(f""">> By PRIME
+Kullanım: python3 {sys.argv[0]} --video video.mp4 : Videoyu karelerine ayırır
+          python3 {sys.argv[0]} --clsoutdir : Çıktı klasörünü temizler.""")
+    exit(0)
+
+
+if len(sys.argv) < 2:
+    ShowHelp()
+
+
+if sys.argv[1] == "--help":
+    ShowHelp()
+
+
+
+
+if sys.argv[1] == "--clsoutdir":
+    print(f"[+] {OUTPUT_DIR} içeriği temizleniyor...")
+    for element in os.listdir(OUTPUT_DIR):
+        os.remove(f"{OUTPUT_DIR}{os.sep}{element}")
+    print("[+] Bitti... ")
+    exit(0)
+
+
+if sys.argv[1] ==  "--video" and os.path.exists(sys.argv[2]):
+    Target = sys.argv[2]
+
+print(">> By PRIME")
+
+
+if not os.path.exists(TMP_DIR) or not os.path.isdir(TMP_DIR):
+    os.mkdir(TMP_DIR)
+
+if not os.path.exists(OUTPUT_DIR) or not os.path.isdir(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
+else:
+    print(f"[+] {OUTPUT_DIR} içeriği temizleniyor...")
+    for element in os.listdir(OUTPUT_DIR):
+        os.remove(f"{OUTPUT_DIR}{os.sep}{element}")
+
+
+
+
+TargetVideo = cv2.VideoCapture(Target)
+FrameNumber = 0
+
+
+
+print("[+] Videodan kareler ayıklanıyor...")
+while(True):
+    is_succes, now_frame = TargetVideo.read()
+    sys.stdout.write(f"\r> Kare: {FrameNumber}")
+    sys.stdout.flush()
+    if FrameNumber % 2 == 0:
+        FrameNumber = FrameNumber + 1
+        continue
+    if is_succes:
+        cv2.imwrite(f"tmp{os.sep}cv2output{os.sep}frame_{FrameNumber}.jpg", now_frame)
+    else:
+        break
+
+    FrameNumber = FrameNumber + 1
+print("[+] Bitti...")
+TargetVideo.release()
